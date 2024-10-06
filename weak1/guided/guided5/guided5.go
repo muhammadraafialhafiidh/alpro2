16b1b244b47951a304119be4c621fa17b0b6268d
muhammadraafialhafiidh <93514919+muhammadraafialhafiidh@users.noreply.github.com>
2024-10-06 12:26:21 +0700

    Add files via upload

---
+++ weak1/guided/guided5/guided5.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var integers [5]int
+	var chars [3]rune
+
+	fmt.Println("Masukkan 5 buah data integer (32-127):")
+	for i := 0; i < 5; i++ {
+		fmt.Scan(&integers[i])
+	}
+
+	fmt.Println("Masukkan 3 buah karakter:")
+	for i := 0; i < 3; i++ {
+		fmt.Scanf("%c", &chars[i])
+	}
+
+	fmt.Println("\nKeluaran:")
+	for i := 0; i < 5; i++ {
+		fmt.Printf("%c", rune(integers[i]))
+	}
+	fmt.Println()
+	for i := 0; i < 3; i++ {
+		fmt.Printf("%c", chars[i])
+	}
+	fmt.Println()
+}
